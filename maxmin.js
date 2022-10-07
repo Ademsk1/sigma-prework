558a084c980655096b215eba34f8ac9441134666
Ademsk1 <85499621+Ademsk1@users.noreply.github.com>
2022-10-07 10:51:54 +0100

    Create maxmin.js
    This is a hard-coded version of finding the maximum and minimum values within an array list (array1).

---
+++ maxmin.js
@@ -0,0 +1,16 @@
+function maxmin(array1) {
+    let myMax = array1[0]
+    let myMin = array1[0]
+    array1.forEach(function findMaxAndMin(el,index,all) {
+        if (myMax < el) {
+            myMax = el;
+        }
+        if (myMin > el) {
+            myMin = el;
+        }
+    })
+    return [myMin,myMax];
+}
+array1 = Array.from({length:10}, () => Math.floor(Math.random() *100);
+console.log(array1);
+console.log(maxmin(array1));
